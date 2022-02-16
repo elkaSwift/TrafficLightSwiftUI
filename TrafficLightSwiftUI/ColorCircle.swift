5afc41eb2bac20eed6be9051a51de9f64999b62e
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-16 21:58:36 +0300

    Adding a variable that will contain opacity values

--- TrafficLightSwiftUI/ColorCircle.swift
+++ TrafficLightSwiftUI/ColorCircle.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 struct ColorCircle: View {
     let color: Color
+    var opacity: CGFloat = 0.5
     
     var body: some View {
         Circle()
             .foregroundColor(color)
+            .opacity(opacity)
             .frame(width: 150, height: 150)
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 10)
+            
     }
 }
 
 struct ColorCircle_Previews: PreviewProvider {
     static var previews: some View {
-        ColorCircle(color: .black)
+        ColorCircle(color: .black, opacity: 0.5)
     }
 }
